d45159ae1852b087e1fa3963d9235dcdc46544e8
Dana Al-Mahrouk <97465951+Almahrouk@users.noreply.github.com>
2026-06-22 20:27:21 +0300

    q

--- nginx-1.31.1/nginx-1.31.1/var/www/cgi/cgi-bin/hello.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo ""
-echo "Hello from CGI"
-echo ""
-echo "Request info:"
-echo "  Method: ${REQUEST_METHOD}"
-echo "  URI: ${REQUEST_URI}"
-echo "  Query: ${QUERY_STRING}"
--- nginx-1.31.1/nginx-1.31.1/var/www/uploads/hello.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-print("Content-Type: text/plain")
-print()
-# while(1)
-print("Hello from CGI")
